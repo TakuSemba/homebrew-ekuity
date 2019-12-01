dc88453309c8eda6346bb81430be6daf86a698a0
TakuSemba <takusemba.ele@gmail.com>
2019-12-01 20:59:18 +0900

    Add files via upload

---
+++ ekuity.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Ekuity < Formula
+  desc "A command-line tool written in Kotlin for calculating equity"
+  homepage "https://github.com/TakuSemba/Ekuity"
+  url "https://github.com/TakuSemba/Ekuity/archive/1.0.0.zip"
+  sha256 "cd6adb9e0f57aab202f5f1301c81ef1a9832a00514a4d463a02b8981aee207f9"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Ekuity`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
