5abdf95956b269108c51112c1ba58f3befdaf206
TakuSemba <takusemba.ele@gmail.com>
2019-12-02 07:38:06 +0900

    Update ekuity.rb

--- Formula/ekuity.rb
+++ Formula/ekuity.rb
@@ -13,7 +13,7 @@ class Ekuity < Formula
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    prefix.install "**/*.jar"
+    lib.install "lib/ekuity.jar"
     bin.install "bin/ekuity"
     # system "cmake", ".", *std_cmake_args
   end
